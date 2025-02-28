ea53b687dd0f9d538240b00efb18215d5c934c7a
majmehdi <majmehdi20@gmail.com>
2025-02-28 21:01:16 +0000

    adding the passenger and the driver method to the moddel Ride and filling the

--- app/Models/Ride.php
+++ app/Models/Ride.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Ride extends Model
 {
     use HasFactory;
+    protected $table = 'ride';
+
+    protected $fillable = [
+        'passenger_id',
+        'driver_id',
+        'reservation_date',
+        'pickup_location',
+        'dropoff_location',
+        'reservation_status',
+        'ride_status',
+    ];
+
+    /**
+     * Get the passenger that owns the ride.
+     */
+    public function passenger()
+    {
+        return $this->belongsTo(Passenger::class);
+    }
+
+    /**
+     * Get the driver that owns the ride.
+     */
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class);
+    }
 }
