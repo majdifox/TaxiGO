83051b8438d2c1cb790a59ece79672363bed2731
majmehdi <majmehdi20@gmail.com>
2025-02-28 21:00:10 +0000

    fixing the Passener modal adding the user and rides methods like the driver modal

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Passenger extends Model
 {
     use HasFactory;
+    protected $fillable = [];
+
+    /**
+     * Get the user that owns the passenger.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the rides for the passenger.
+     */
+    public function rides()
+    {
+        return $this->hasMany(Ride::class);
+    }
 }
