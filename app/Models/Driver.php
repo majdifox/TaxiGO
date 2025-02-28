74d58927893a4d62d80fc9364104e381eac7c10e
majmehdi <majmehdi20@gmail.com>
2025-02-28 20:59:12 +0000

    fixing the driver modal fixing adding the user and rides methods

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     use HasFactory;
+    protected $fillable = [];
+
+    /**
+     * Get the user that owns the driver.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the rides for the driver.
+     */
+    public function rides()
+    {
+        return $this->hasMany(Ride::class);
+    }
 }
